9841d599a2fdfa57a51dbc1f78960129876e18be
lexa-makeev <makeevip@yandex.ru>
2023-04-16 01:50:13 +0300

    refactor

--- index.js
+++ index.js
@@ -1,61 +1,63 @@
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-
-const db = require("./models")
-const {User} = require("./models")
+const db = require("./models");
+const { User } = require("./models");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/select", (req, res) => {
-    User.findAll().then((users) => {
-        res.send(users)
-    }).catch((err) => {
-        console.log(err)
+  User.findAll()
+    .then((users) => {
+      res.send(users);
     })
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
 app.post("/reg", (req, res) => {
-    const {email, pass, role, name, fam, otch} = req.body;
-    User.create({
-        email: email,
-        pass: pass,
-        role: role,
-        name: name,
-        fam: fam,
-        otch: otch,
-
-    }).then(() => {
-        res.send("insert");
-    }).catch((err) => {
-        console.log(err)
-        res.status(500).send("Ошибка при сохранении пользователя");
+  const { email, pass, role, name, fam, otch } = req.body;
+  User.create({
+    email: email,
+    pass: pass,
+    role: role,
+    name: name,
+    fam: fam,
+    otch: otch,
+  })
+    .then(() => {
+      res.send("insert");
     })
-
-})
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Ошибка при сохранении пользователя");
+    });
+});
 app.post("/auth", (req, res) => {
-    const {email, pass} = req.body;
-    User.findOne({where: {email: email}}).then((user) => {
-        if (user !== null && user.pass === pass) {
-            res.send("true");
-        } else {
-            res.status(401).send("Неверный пароль");
-        }
-    }).catch((err) => {
-        res.status(404).send("Пользователя не найдено");
-        console.log(err)
+  const { email, pass } = req.body;
+  User.findOne({
+    where: {
+      email: email,
+    },
+  })
+    .then((user) => {
+      if (user !== null && user.pass === pass) {
+        res.send("true");
+      } else {
+        res.status(401).send("Неверный пароль");
+      }
     })
-})
-
+    .catch((err) => {
+      res.status(404).send("Пользователя не найдено");
+      console.log(err);
+    });
+});
 
 db.sequelize.sync().then((req) => {
-    app.listen(3001, () => {
-        console.log("Запустился на сервере 3001")
-    })
-
-})
-
-
-
+  app.listen(3001, () => {
+    console.log("Запустился на сервере 3001");
+  });
+});
